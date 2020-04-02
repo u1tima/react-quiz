c1bced3b5072bac3950b0643c22c50393a1dd53e
Vitali Semashkevich <vit-sema@yandex.ru>
2020-04-02 18:20:36 +0300

    [dev] add pages

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './QuizList.module.css';
+
+class QuizList extends Component {
+  render() {
+    return (
+      <div className={classes.QuizList}>
+        <h1>Quiz List</h1>
+      </div>
+    );
+  }
+}
+
+export default QuizList;
