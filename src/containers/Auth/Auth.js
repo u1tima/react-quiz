1436c93864e22c74be33cc2f0c95fe224f242e64
Vitali Semashkevich <vit-sema@yandex.ru>
2020-04-03 13:15:42 +0300

    [dev] add controls to auth page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form
+            className={classes.AuthForm}
+            onSubmit={this.submitHandler}>
+            <Input
+              label='Email'
+              errorMessage='Test'
+            />
+            <Input label='Пароль' />
+            <Button type='succsess' onCick={this.loginHandler} >Войти</Button>
+            <Button type='primary' onClick={this.registerHandler}>Зарегистрироваться</Button>
+
+          </form>
+        </div>
       </div>
     );
   }
